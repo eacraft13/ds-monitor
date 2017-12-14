e37c0d4e63ecba16e749890215b6c91883a33bc6
eric <eacraft13@gmail.com>
2017-12-13 23:36:44 -0600

    Add dynamic styles to nav

--- src/app/app.js
+++ src/app/app.js
@@ -5,12 +5,21 @@
 $(function () {
 
     var $menus = $('.menu');
+    var $nav = $('header nav > span');
 
     /**
      * Close all menus on click
      */
     $('html').on('click', function () {
         $menus.hide('fast');
+    });
+
+    /**
+     * Navigation
+     */
+    $nav.on('click', function () {
+        $nav.removeClass('active');
+        $(this).addClass('active');
     });
 
     /**
